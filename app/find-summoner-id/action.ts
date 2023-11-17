b26da0ab06925579b659cea435616ccf20497c37
Pierre Charpentier <charpentier.pierre@outlook.com>
2023-11-17 19:45:11 +0100

    Make sure actions are server side only

--- app/find-summoner-id/action.ts
+++ app/find-summoner-id/action.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { getSummonerDataFromSummonerName } from "@/app/lib/riot-games-api";
 import { z } from "zod";
 
@@ -5,10 +7,6 @@ export async function handleFindSummonerIdForm(
   previousState: { summonerId: string | null },
   formData: FormData
 ) {
-  const schema = z.object({
-    summonerName: z.string().min(1),
-  });
-
   try {
     const summonerName = z.string().min(1).parse(formData.get("summoner-name"));
     const summonerData = await getSummonerDataFromSummonerName(summonerName);
