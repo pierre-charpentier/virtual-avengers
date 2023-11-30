b32e6f09a3dc0c19347feac127b3f2eb958d8411
Pierre Charpentier <charpentier.pierre@outlook.com>
2023-11-30 09:52:53 +0100

    Fix cache timers for Riot Games API

--- app/lib/riot-games-api.ts
+++ app/lib/riot-games-api.ts
@@ -1,18 +1,22 @@
 import Env from "@/app/lib/env";
 
 const API_KEY = Env.RIOT_GAMES_API_KEY;
-const BASE_RANKED_URL =
-  "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
-const BASE_URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/";
+
+const RG_API_URLS = {
+  LEAGUE: "https://euw1.api.riotgames.com/lol/league/v4/entries/",
+  SUMMONER: "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/",
+};
 
 export async function getRankedDataForSummoner(
   summonerId: string
 ): Promise<ReadonlyArray<RiotGamesAPI.League.LeagueEntryDto>> {
-  const url = new URL(summonerId, BASE_RANKED_URL);
+  const url = new URL(`by-summoner/${summonerId}`, RG_API_URLS.LEAGUE);
 
   url.searchParams.append("api_key", API_KEY);
 
-  const request = await fetch(url, { next: { revalidate: 60 * 60 } });
+  const request = await fetch(url, {
+    next: { revalidate: 3600 /* an hour */ },
+  });
 
   return await request.json();
 }
@@ -20,11 +24,13 @@ export async function getRankedDataForSummoner(
 export async function getSummonerDataFromSummonerName(
   summonerName: string
 ): Promise<RiotGamesAPI.Summoner.SummonerDto> {
-  const url = new URL(`by-name/${summonerName}`, BASE_URL);
+  const url = new URL(`by-name/${summonerName}`, RG_API_URLS.SUMMONER);
 
   url.searchParams.append("api_key", API_KEY);
 
-  const request = await fetch(url, { next: { revalidate: 60 * 60 } });
+  const request = await fetch(url, {
+    next: { revalidate: 3600 /* an hour */ },
+  });
 
   return await request.json();
 }
@@ -32,11 +38,13 @@ export async function getSummonerDataFromSummonerName(
 export async function getSummonerDataFromSummonerId(
   summonerId: string
 ): Promise<RiotGamesAPI.Summoner.SummonerDto> {
-  const url = new URL(summonerId, BASE_URL);
+  const url = new URL(summonerId, RG_API_URLS.SUMMONER);
 
   url.searchParams.append("api_key", API_KEY);
 
-  const request = await fetch(url, { next: { revalidate: 60 * 60 * 24 } });
+  const request = await fetch(url, {
+    next: { revalidate: 86400 /* one day */ },
+  });
 
   return await request.json();
 }
